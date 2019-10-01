f5009f1450c56ad4e954876bb034c73ca27a028e
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-10-01 12:44:57 -0400

    artist model can get song, genre count, and first

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,21 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    # Genre.all.each do |genres|
+    #   genres
+    # end.first
+    self.genres.first
   end
 
   def song_count
     #return the number of songs associated with the artist
+    # Song.count
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    # self.songs.count
+    self.genres.count
   end
 end
